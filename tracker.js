4c51a2ab2789f8ed642787b30749310fe03c74d0
junerish <junerish@gmail.com>
2026-07-20 21:19:58 +0900

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -1,9 +1,9 @@
 (function(){
   var EP = window.PORTFOLIO_ANALYTICS_ENDPOINT || (function(){
     var s = localStorage.getItem('pf_endpoint');
-    if (s && s.indexOf('AKfycbxkYqQVIFUj94LvY_L_cfm-SoVL7F2dGuW157ezBzEkrrPnnT8I-BYZLzxdw4ei49Oevg') > -1) { s = null; localStorage.removeItem('pf_endpoint'); }
+    if (s && (s.indexOf('AKfycbxkYqQVIFUj94LvY_L_cfm-SoVL7F2dGuW157ezBzEkrrPnnT8I-BYZLzxdw4ei49Oevg') > -1 || s.indexOf('AKfycbxUQZeTza47oB-GAYb7l7lTqrBEunse0WpW_tTrqplHEGjBe-nAcMPkSvsX4YuoTUob') > -1)) { s = null; localStorage.removeItem('pf_endpoint'); }
     return s;
-  })() || 'https://script.google.com/macros/s/AKfycbxUQZeTza47oB-GAYb7l7lTqrBEunse0WpW_tTrqplHEGjBe-nAcMPkSvsX4YuoTUob/exec';
+  })() || 'https://script.google.com/macros/s/AKfycbwhJ_cQs5hDh-FpvmxJd6ZeLk2Bk_U0zpx2wPhGhfNC0V5L45ki5pea6gsKgLr-qor86w/exec';
   function save(v){
     try{ var a=JSON.parse(localStorage.getItem('pf_visits')||'[]'); a.push(v); if(a.length>5000)a=a.slice(-5000); localStorage.setItem('pf_visits',JSON.stringify(a)); }catch(e){}
     if(EP){ try{ var body=JSON.stringify(v); if(navigator.sendBeacon){ navigator.sendBeacon(EP, body); } else { fetch(EP,{method:'POST',mode:'no-cors',headers:{'Content-Type':'text/plain'},body:body}); } }catch(e){} }
